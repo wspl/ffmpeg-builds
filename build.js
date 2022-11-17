cbe83c63cfa325d10e109eb010c897371ad064ce
wspl <widesense.plqws@gmail.com>
2022-11-17 23:54:50 +0800

    test

--- build.js
+++ build.js
@@ -1,53 +1,32 @@
 const { execSync, spawnSync } = require('child_process')
 const fs = require('fs')
 
+// setup msys env
 const msysDir = execSync('msys2 -c "cygpath -m /"').toString().trim()
 const msysBinDir = `${msysDir.replaceAll('/', '\\')}\\usr\\bin`
 
-console.log(fs.readdirSync(msysBinDir).join('\n'))
-
-// setup vs
+// setup vs env
 fs.writeFileSync('temp.bat', `call "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Auxiliary/Build/vcvarsall.bat" amd64\nset`)
 const sets = spawnSync('temp.bat', {
   shell: true
 }).stdout.toString()
 fs.rmSync('temp.bat')
-console.log(sets)
 const vsEnvs = Object.fromEntries(sets.split('\n').slice(5).map(line => line.trim().split('=')).filter(t => t.length > 1))
 
-execSync('git clone https://github.com/FFmpeg/FFmpeg.git', {
-  stdio: 'inherit'
-})
+// build
+const options = {
+  stdio: 'inherit',
+  cwd: 'FFmpeg',
+  env: {
+    ...vsEnvs,
+    Path: `${msysBinDir};${vsEnvs.Path}`
+  }
+}
 
 spawnSync('sh', [
   './configure',
   '--toolchain=msvc',
   '--prefix=../output'
-], {
-  stdio: 'inherit',
-  cwd: 'FFmpeg',
-  env: {
-    ...vsEnvs,
-    Path: `${msysBinDir};${vsEnvs.Path}`
-  }
-})
-
-execSync(`${msysBinDir}\\make.exe`, {
-  stdio: 'inherit',
-  cwd: 'FFmpeg',
-  env: {
-    ...vsEnvs,
-    Path: `${msysBinDir};${vsEnvs.Path}`
-  }
-})
-
-execSync(`${msysBinDir}\\make.exe install`, {
-  stdio: 'inherit',
-  cwd: 'FFmpeg',
-  env: {
-    ...vsEnvs,
-    Path: `${msysBinDir};${vsEnvs.Path}`
-  }
-})
-
-console.log(fs.readFileSync('FFmpeg/ffbuild/config.log', 'utf-8'))
+], options)
+execSync(`${msysBinDir}\\make.exe`, options)
+execSync(`${msysBinDir}\\make.exe install`, options)
