98488671d594e334a155a308dd871549aa856c4c
wspl <widesense.plqws@gmail.com>
2022-11-18 22:47:09 +0800

    test

--- build.js
+++ build.js
@@ -47,11 +47,8 @@ function buildDarwin(arch) {
     '--enable-cross-compile',
     `--arch=${arch}`,
     '--cc=clang',
-    // '--as=llvm-as',
-    // '--ld=clang',
-    `--extra-cflags="--target=${arch}-apple-darwin -march=${arch}"`,
-    `--extra-ldflags="--target=${arch}-apple-darwin -march=${arch}"`,
-    // `--extra-asflags='--target=${arch}-apple-darwin -march=${arch}'`
+    `--extra-cflags="--target=${arch}-apple-darwin"`,
+    `--extra-ldflags="--target=${arch}-apple-darwin"`,
   ].join(' '), options)
   console.log(fs.readFileSync('FFmpeg/ffbuild/config.log', 'utf-8'))
   execSync(`make -j16`, options)
